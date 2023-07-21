ca1e7113ddd8b257ee5c5874e09dffb0b9da84c2
Mehmet Karahan <wiruzman@gmail.com>
2023-07-22 01:42:35 +0200

    add rainmeter plugin and console application to read sensor identities

---
+++ C#/ConsoleLibreHardwareMonitor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using LibreHardwareMonitor.Hardware;
+
+namespace ConsoleLibreHardwareMonitor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+
+            var sensors = computer.Hardware.SelectMany(h => h.Sensors.Concat(h.SubHardware.SelectMany(s => s.Sensors)));
+            foreach (var sensor in sensors)
+            {
+                Console.WriteLine("\t\tSensor: {0}, value: {1}, identifier: {2}", sensor.Name, sensor.Value, sensor.Identifier);
+            }
+    
+            computer.Close();
+        }
+        
+        private class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+        }
+    }
+}
